a7f2045ae2e21aeb8af63dc5423d9068b8488d40
nicole-perez-site <nicperez@csumb.edu>
2024-11-26 18:45:45 -0800

    updates

---
+++ bbpandppemodule/Basics of the OR Module/story_content/6CHvXtAVIO7_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":3616,"text":"Turn on the water"},{"start":10048,"text":"Get soap from the dispenser "},{"start":16096,"text":"scrub inbetween all fingers"},{"start":22144,"text":"wash your thumb"},{"start":28192,"text":"wash your wrist"},{"start":34240,"text":"wash the rest of your hands (front and back)"},{"start":40288,"text":"rinse off the soap"},{"start":46336,"text":"dry hands"}]}]};
+    window.globalLoadJsAsset('story_content/6CHvXtAVIO7_transcripts.js', JSON.stringify(data));
+})();
