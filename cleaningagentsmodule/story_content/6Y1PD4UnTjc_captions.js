b7fc48956095f45220399d26cbb2944406cbdb07
nicole-perez-site <nicperez@csumb.edu>
2024-12-01 21:55:19 -0800

    updatedcleaningandbbp

---
+++ cleaningagentsmodule/story_content/6Y1PD4UnTjc_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.93.33359.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:02.711%0D%0APress%20play%20to%20watch%20the%203M%20dispenser%20in%20use.%0D%0A%0D%0A"}')
